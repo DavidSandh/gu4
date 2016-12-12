626910a3f3ed3ed76b786c69e8ce35314dbf7b6c
Lucas <lucasknutsater@gmail.com>
2016-12-12 14:31:41 +0100

    setCol och getCol

--- Array7x7.java
+++ Array7x7.java
@@ -12,13 +12,26 @@ public class Array7x7 {
 	}
 	public void setRow(int row, Array7 theRow) {
 		for(int i = 0; i < 7; i ++){
-			arr[row][i] = (theRow.getElement(i));
+			arr[row][i] = theRow.getElement(i);
 		}
 	}
 	public Array7 getRow(int row) {
 		Array7 arr7 = new Array7();
 		for(int i = 0; i < 7; i++) {
 			arr7.setElement(i, arr[row][i]);
+		}
+		return arr7;
+	}
+	public void setCol(int col, Array7 theCol) {
+		for(int i = 0; i < 7; i++) {
+			arr[i][col] = theCol.getElement(i);
+		}
+	}
+	
+	public Array7 getCol(int col) {
+		Array7 arr7 = new Array7();
+		for(int i = 0; i < 7; i++) {
+			arr7.setElement(i, arr[col][i]);
 		}
 		return arr7;
 	}
