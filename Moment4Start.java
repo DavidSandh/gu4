03c4431efc0e5f60380dd6f6aa83085d76c66fd4
Lucas <lucasknutsater@gmail.com>
2016-12-19 14:24:44 +0100

    sliders

--- Moment4Start.java
+++ Moment4Start.java
@@ -1,23 +1,9 @@
 package gu4;
 
-import java.awt.Dimension;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class Moment4Start extends JPanel {
+public class Moment4Start{
 	public static void main(String[] args) {
 		Moment4 m4 = new Moment4(1,5,Color.WHITE,Color.BLACK,1,10);
-		//Moment4Viewer m4Viewer = new Moment4Viewer(m4);
-		JFrame frame = new JFrame( "Moment4" );
-		frame.setPreferredSize(new Dimension(800,800));
-		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		frame.add( m4 );
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		Moment4Viewer m4Viewer = new Moment4Viewer(m4,frame);
-		frame.setVisible( true );
-		m4.Test();
+		Moment4Viewer m4Viewer = new Moment4Viewer(m4);
 	}
 
 }
--- Moment4Viewer.java
+++ Moment4Viewer.java
@@ -1,27 +1,49 @@
 package gu4;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.Dictionary;
 
 import javax.swing.*;
 
 public class Moment4Viewer extends JComponent {
 	private Moment4 listener;
-	private JFrame frame;
 	private JSlider sliderR = new JSlider(JSlider.HORIZONTAL,0,250,125);
 	private JSlider sliderB = new JSlider(JSlider.HORIZONTAL,0,250,125);
 	private JSlider sliderG = new JSlider(JSlider.HORIZONTAL,0,250,125);
 	private JTextField tfInput = new JTextField();
 	private JButton btnSlumpa = new JButton("Slumpa");
 	private JPanel panelSliders = new JPanel(new BorderLayout());
+	private JPanel panel = new JPanel(new BorderLayout());
+	private JLabel red = new JLabel("RÖD");
+	private JLabel blue = new JLabel("BLÅ");
+	private JLabel green = new JLabel("GRÖN");
+	private JPanel labelPanel = new JPanel(new BorderLayout());
+	private JPanel sliders = new JPanel(new BorderLayout());
+	
+	private JFrame frame = new JFrame( "Moment4" );
 	
 
-	public Moment4Viewer(Moment4 listener,JFrame frame) {
+	public Moment4Viewer(Moment4 listener) {
 		this.listener = listener;
-		this.frame = frame;
-		panelSliders.add(sliderR,BorderLayout.NORTH);
-		panelSliders.add(sliderB, BorderLayout.CENTER);
-		panelSliders.add(sliderG, BorderLayout.SOUTH);
-		frame.add(panelSliders);
+		//labelPanel.setPreferredSize(new Dimension(50,50));
+		labelPanel.add(red, BorderLayout.NORTH);
+		labelPanel.add(blue, BorderLayout.CENTER);
+		labelPanel.add(green, BorderLayout.SOUTH);
+		sliders.add(sliderR, BorderLayout.NORTH);
+		sliders.add(sliderB, BorderLayout.CENTER);
+		sliders.add(sliderG, BorderLayout.SOUTH);
+		panelSliders.add(labelPanel, BorderLayout.WEST);
+		panelSliders.add(sliders,BorderLayout.CENTER);
+		panel.add(panelSliders, BorderLayout.SOUTH);
+		panel.add(listener, BorderLayout.CENTER);
+		frame.add(panel);
+		frame.setPreferredSize(new Dimension(800,800));
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible( true );
+		listener.Test();
 	
 		
 	}
